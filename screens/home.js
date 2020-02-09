f21a82cbc7176985d9e1890649c53bd940fcc950
jamiebdavis <jamied1985@gmail.com>
2020-02-09 21:24:47 +0000

    first commit

---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text} from "react-native"
+
+const MyComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyComponent;
