83da95cce0f2f208a0fac6a9388b23a5193f9b63
jamiebdavis <jamied1985@gmail.com>
2020-02-24 22:08:57 +0000

    added custom button

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
-import {StyleSheet, View, Text, Button, FlatList, TouchableOpacity} from "react-native";
-import {globalStyles} from "../styles/global";
+import React, { useState } from "react";
+import {
+    StyleSheet,  View,  Text,   FlatList, TouchableOpacity, Modal, TouchableWithoutFeedBack  } from "react-native";
+import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
+import { MaterialIcons } from "expo/vector-icons";
+import ReviewForm from "./reviewForm";
 
-export default function Home({navigation}) {
+
+export default function Home({ navigation }) {
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews, setReviews] = useState([
         {
             title: "Zelda, Breath of Fresh Air",
@@ -24,16 +30,67 @@ export default function Home({navigation}) {
         }
     ]);
 
+    const addReview = review => {
+        review.key = Math.random().toString();
+        setReviews(currentReviews => {
+            return [review, ...reviews];
+        });
+        setModalOpen(false);
+    };
+
     return (
         <View style={globalStyles.container}>
+            <TouchableWithoutFeedBack onPress={Keyboard.dismiss}>
+                <Modal visable={modalOpen} animationType="slide">
+                    <View style={styles.modelContent}>
+                        <MaterialIcons
+                            name="close"
+                            size={24}
+                            onPress={() => setModalOpen(false)}
+                            style={{ ...styles.modalToggle, ...styles.modalClose }}
+                        />
+                        <ReviewForm addReview={addReview} />
+                    </View>
+                </Modal>
+            </TouchableWithoutFeedBack>
+
+            <MaterialIcons
+                name="add"
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
+
             <FlatList
                 data={reviews}
-                renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)} >
-                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("ReviewDetails", item)}
+                    >
+                        <Card>
+                            <Text style={globalStyles.titleText}>{item.title}</Text>
+                        </Card>
                     </TouchableOpacity>
                 )}
             />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColour: "#f2f2f2",
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: "center"
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    },
+    modelContent: {
+        flex: 1
+    }
+});
